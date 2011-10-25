0526d2e0dfd284a064868d1e865801e047c1b545
David Cramer <dcramer@gmail.com>
2011-10-25 16:18:19 -0700

    Initial commit with fix for utf8prober when char is out of range

---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+
+# patch distutils if it can't cope with the "classifiers" or "download_url"
+# keywords (prior to python 2.3.0).
+from distutils.dist import DistributionMetadata
+if not hasattr(DistributionMetadata, 'classifiers'):
+    DistributionMetadata.classifiers = None
+if not hasattr(DistributionMetadata, 'download_url'):
+    DistributionMetadata.download_url = None
+    
+setup(
+    name = 'chardet',
+    version = '1.0.1',
+    description = 'Universal encoding detector',
+    long_description = """\
+Universal character encoding detector
+-------------------------------------
+
+Detects
+ - ASCII, UTF-8, UTF-16 (2 variants), UTF-32 (4 variants)
+ - Big5, GB2312, EUC-TW, HZ-GB-2312, ISO-2022-CN (Traditional and Simplified Chinese)
+ - EUC-JP, SHIFT_JIS, ISO-2022-JP (Japanese)
+ - EUC-KR, ISO-2022-KR (Korean)
+ - KOI8-R, MacCyrillic, IBM855, IBM866, ISO-8859-5, windows-1251 (Cyrillic)
+ - ISO-8859-2, windows-1250 (Hungarian)
+ - ISO-8859-5, windows-1251 (Bulgarian)
+ - windows-1252 (English)
+ - ISO-8859-7, windows-1253 (Greek)
+ - ISO-8859-8, windows-1255 (Visual and Logical Hebrew)
+ - TIS-620 (Thai)
+
+Requires Python 2.1 or later
+""",
+    author='Mark Pilgrim',
+    author_email = 'mark@diveintomark.org',
+    url = 'http://chardet.feedparser.org/',
+    download_url = 'http://chardet.feedparser.org/download/',
+    license = "LGPL",
+    platforms = ['POSIX', 'Windows'],
+    keywords = ['encoding', 'i18n', 'xml'],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+        ],
+    packages = ['chardet']
+    )
