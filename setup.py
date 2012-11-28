d2a5dcef92ee0fd70e73bf4639d42c99346fb899
Ian Cordasco <graffatcolmingov@gmail.com>
2012-11-27 23:01:27 -0500

    A new start.

--- setup.py
+++ setup.py
@@ -9,56 +9,27 @@ if not hasattr(DistributionMetadata, 'download_url'):
     DistributionMetadata.download_url = None
 
 setup(
-    name = 'chardet',
-    version = '1.1',
-    description = 'Universal encoding detector',
-    long_description = """\
-Universal character encoding detector
--------------------------------------
-
-Detects
- - ASCII, UTF-8, UTF-16 (2 variants), UTF-32 (4 variants)
- - Big5, GB2312, EUC-TW, HZ-GB-2312, ISO-2022-CN (Traditional and Simplified Chinese)
- - EUC-JP, SHIFT_JIS, ISO-2022-JP (Japanese)
- - EUC-KR, ISO-2022-KR (Korean)
- - KOI8-R, MacCyrillic, IBM855, IBM866, ISO-8859-5, windows-1251 (Cyrillic)
- - ISO-8859-2, windows-1250 (Hungarian)
- - ISO-8859-5, windows-1251 (Bulgarian)
- - windows-1252 (English)
- - ISO-8859-7, windows-1253 (Greek)
- - ISO-8859-8, windows-1255 (Visual and Logical Hebrew)
- - TIS-620 (Thai)
-
-Requires Python 2.1 or later
-
-Command-line Tool
------------------
-
-chardet comes with a command-line script which reports on the encodings of one
-or more files::
-
-    % chardetect.py somefile someotherfile
-    somefile: windows-1252 with confidence 0.5
-    someotherfile: ascii with confidence 1.0
-
-""",
+    name='charade',
+    version='1.1',
+    description='Universal encoding detector',
+    long_description=open('README.rst').read(),
     author='Mark Pilgrim',
-    author_email = 'mark@diveintomark.org',
-    url = 'http://chardet.feedparser.org/',
-    download_url = 'http://chardet.feedparser.org/download/',
-    license = "LGPL",
-    platforms = ['POSIX', 'Windows'],
-    keywords = ['encoding', 'i18n', 'xml'],
-    classifiers = [
+    author_email='mark@diveintomark.org',
+    url='https://github.com/sigmavirus24/charade',
+    license="LGPL",
+    platforms=['POSIX', 'Windows'],
+    keywords=['encoding', 'i18n', 'xml'],
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "License :: OSI Approved :: GNU Library or Lesser General Public"
+        " License (LGPL)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Text Processing :: Linguistic",
-        ],
+    ],
     scripts=['bin/chardetect.py'],
-    packages = ['chardet']
-    )
+    packages=['chardet']
+)
