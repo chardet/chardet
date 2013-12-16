2bba0078ca8900fbb8803ce81c178df10b477bf9
Dan Blanchard <dblanchard@ets.org>
2013-12-16 09:34:45 -0500

    Slightly reformat setup.py and switch to safer readme reading.

--- setup.py
+++ setup.py
@@ -11,35 +11,35 @@ if not hasattr(DistributionMetadata, 'classifiers'):
 if not hasattr(DistributionMetadata, 'download_url'):
     DistributionMetadata.download_url = None
 
-package = ['chardet']
-script = ['bin/chardetect']
 
 from chardet import __version__
 
-setup(
-    name='chardet',
-    version=__version__,
-    description='Universal encoding detector for python 2 and 3',
-    long_description='\n\n'.join([open('README.rst').read(),
-                                  open('HISTORY.rst').read()]),
-    author='Mark Pilgrim',
-    author_email='mark@diveintomark.org',
-    maintainer='Ian Cordasco',
-    maintainer_email='graffatcolmingov@gmail.com',
-    url='https://github.com/erikrose/chardet',
-    license="LGPL",
-    keywords=['encoding', 'i18n', 'xml'],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Other Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Library or Lesser General Public"
-        " License (LGPL)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Text Processing :: Linguistic",
-    ],
-    scripts=script,
-    packages=package,
-)
+
+def README():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='chardet',
+      version=__version__,
+      description='Universal encoding detector for Python 2 and 3',
+      long_description=README(),
+      author='Mark Pilgrim',
+      author_email='mark@diveintomark.org',
+      maintainer='Ian Cordasco',
+      maintainer_email='graffatcolmingov@gmail.com',
+      url='https://github.com/erikrose/chardet',
+      license="LGPL",
+      keywords=['encoding', 'i18n', 'xml'],
+      classifiers=["Development Status :: 4 - Beta",
+                   "Environment :: Other Environment",
+                   "Intended Audience :: Developers",
+                   ("License :: OSI Approved :: GNU Library or Lesser General" +
+                    " Public License (LGPL)"),
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   ("Topic :: Software Development :: Libraries :: Python " +
+                    "Modules"),
+                   "Topic :: Text Processing :: Linguistic"],
+      scripts=['bin/chardetect'],
+      packages=['chardet'])
