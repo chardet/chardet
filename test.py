6ca230c5cf7d94d9e89be77cd7f358cb35b96321
Dan Blanchard <dan.blanchard@gmail.com>
2017-04-10 16:50:26 -0400

    Skip hypothesis testing on Python 2.6 since it is no longer supported. Would love to remove support for 2.6 from chardet as well.

--- test.py
+++ test.py
@@ -13,9 +13,13 @@ from io import open
 from os import listdir
 from os.path import dirname, isdir, join, realpath, relpath, splitext
 
-import hypothesis.strategies as st
+try:
+    import hypothesis.strategies as st
+    from hypothesis import given, assume, settings, Verbosity
+    HAVE_HYPOTHESIS = True
+except ImportError:
+    HAVE_HYPOTHESIS = False
 import pytest
-from hypothesis import given, assume, settings, Verbosity
 
 import chardet
 
@@ -93,31 +97,32 @@ def test_encoding_detection(file_name, encoding):
                                                    diff))
 
 
-class JustALengthIssue(Exception):
-    pass
+if HAVE_HYPOTHESIS:
+    class JustALengthIssue(Exception):
+        pass
 
 
-@pytest.mark.xfail
-@given(st.text(min_size=1), st.sampled_from(['ascii', 'utf-8', 'utf-16',
-                                             'utf-32', 'iso-8859-7',
-                                             'iso-8859-8', 'windows-1255']),
-       st.randoms())
-@settings(max_examples=200)
-def test_never_fails_to_detect_if_there_is_a_valid_encoding(txt, enc, rnd):
-    try:
-        data = txt.encode(enc)
-    except UnicodeEncodeError:
-        assume(False)
-    detected = chardet.detect(data)['encoding']
-    if detected is None:
-        with pytest.raises(JustALengthIssue):
-            @given(st.text(), random=rnd)
-            @settings(verbosity=Verbosity.quiet, max_shrinks=0, max_examples=50)
-            def string_poisons_following_text(suffix):
-                try:
-                    extended = (txt + suffix).encode(enc)
-                except UnicodeEncodeError:
-                    assume(False)
-                result = chardet.detect(extended)
-                if result and result['encoding'] is not None:
-                    raise JustALengthIssue()
+    @pytest.mark.xfail
+    @given(st.text(min_size=1), st.sampled_from(['ascii', 'utf-8', 'utf-16',
+                                                 'utf-32', 'iso-8859-7',
+                                                 'iso-8859-8', 'windows-1255']),
+           st.randoms())
+    @settings(max_examples=200)
+    def test_never_fails_to_detect_if_there_is_a_valid_encoding(txt, enc, rnd):
+        try:
+            data = txt.encode(enc)
+        except UnicodeEncodeError:
+            assume(False)
+        detected = chardet.detect(data)['encoding']
+        if detected is None:
+            with pytest.raises(JustALengthIssue):
+                @given(st.text(), random=rnd)
+                @settings(verbosity=Verbosity.quiet, max_shrinks=0, max_examples=50)
+                def string_poisons_following_text(suffix):
+                    try:
+                        extended = (txt + suffix).encode(enc)
+                    except UnicodeEncodeError:
+                        assume(False)
+                    result = chardet.detect(extended)
+                    if result and result['encoding'] is not None:
+                        raise JustALengthIssue()
