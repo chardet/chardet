a49fdf587fbeb4bbe08a00d1777e7d4e0c3f3bfd
Dan Blanchard <dan.blanchard@gmail.com>
2017-04-27 13:33:21 -0400

    Keep Hungarian commented out until we retrain

--- chardet/sbcsgroupprober.py
+++ chardet/sbcsgroupprober.py
@@ -32,8 +32,8 @@ from .langbulgarianmodel import (ISO_8859_5_BULGARIAN_MODEL,
                                  WINDOWS_1251_BULGARIAN_MODEL)
 from .langgreekmodel import ISO_8859_7_GREEK_MODEL, WINDOWS_1253_GREEK_MODEL
 from .langhebrewmodel import WINDOWS_1255_HEBREW_MODEL
-from .langhungarianmodel import (ISO_8859_2_HUNGARIAN_MODEL,
-                                 WINDOWS_1250_HUNGARIAN_MODEL)
+# from .langhungarianmodel import (ISO_8859_2_HUNGARIAN_MODEL,
+#                                  WINDOWS_1250_HUNGARIAN_MODEL)
 from .langrussianmodel import (IBM855_RUSSIAN_MODEL, IBM866_RUSSIAN_MODEL,
                                ISO_8859_5_RUSSIAN_MODEL, KOI8_R_RUSSIAN_MODEL,
                                MACCYRILLIC_RUSSIAN_MODEL,
@@ -70,8 +70,10 @@ class SBCSGroupProber(CharSetGroupProber):
             SingleByteCharSetProber(WINDOWS_1253_GREEK_MODEL),
             SingleByteCharSetProber(ISO_8859_5_BULGARIAN_MODEL),
             SingleByteCharSetProber(WINDOWS_1251_BULGARIAN_MODEL),
-            SingleByteCharSetProber(ISO_8859_2_HUNGARIAN_MODEL),
-            SingleByteCharSetProber(WINDOWS_1250_HUNGARIAN_MODEL),
+            # TODO: Restore Hungarian encodings (iso-8859-2 and windows-1250)
+            #       after we retrain model.
+            # SingleByteCharSetProber(ISO_8859_2_HUNGARIAN_MODEL),
+            # SingleByteCharSetProber(WINDOWS_1250_HUNGARIAN_MODEL),
             SingleByteCharSetProber(TIS_620_THAI_MODEL),
             SingleByteCharSetProber(ISO_8859_9_TURKISH_MODEL),
             hebrew_prober,
