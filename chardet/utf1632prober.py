94869c80dc1c3c1de98142dfab763e979df0614b
Jason Zavaglia <jason.zavaglia@gmail.com>
2017-06-01 01:13:54 +1000

    clean up poorly written comments

--- chardet/utf1632prober.py
+++ chardet/utf1632prober.py
@@ -33,8 +33,8 @@ class UTF1632Prober(CharSetProber):
     This class simply looks for occurrences of zero bytes, and infers
     whether the file is UTF16 or UTF32 (low-endian or big-endian)
     For instance, files looking like ( \0 \0 \0 [nonzero] )+
-    appear to be UTF32LE.  Files looking like ( \0 [notzero] )+ appear
-    may be guessed to be UTF16LE.
+    have a good probability to be UTF32BE.  Files looking like ( \0 [nonzero] )+ 
+    may be guessed to be UTF16BE, and inversely for little-endian varieties.
     """
 
     # how many logical characters to scan before feeling confident of prediction
@@ -129,7 +129,7 @@ class UTF1632Prober(CharSetProber):
 
     def validate_utf32_characters(self, quad):
         """
-        Identify if the quad of bytes is not valid UTF-32.
+        Validate if the quad of bytes is valid UTF-32.
 
         UTF-32 is valid in the range 0x00000000 - 0x0010FFFF
         excluding 0x0000D800 - 0x0000DFFF
@@ -145,7 +145,7 @@ class UTF1632Prober(CharSetProber):
 
     def validate_utf16_characters(self, pair):
         """
-        Identify if the pair of bytes is not valid UTF-16.
+        Validate if the pair of bytes is  valid UTF-16.
 
         UTF-16 is valid in the range 0x0000 - 0xFFFF excluding 0xD800 - 0xFFFF
         with an exception for surrogate pairs, which must be in the range
