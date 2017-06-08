9b8c5c2fb118d76c6beeab9affd01c332732a530
Dan Blanchard <dan.blanchard@gmail.com>
2017-06-08 10:29:16 -0400

    Bump version to 3.0.4

--- chardet/version.py
+++ chardet/version.py
@@ -5,5 +5,5 @@ from within setup.py and from chardet subpackages.
 :author: Dan Blanchard (dan.blanchard@gmail.com)
 """
 
-__version__ = "3.0.3"
+__version__ = "3.0.4"
 VERSION = __version__.split('.')
