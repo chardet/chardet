45c4b796fbcbc2d2889095725103c6de501d5e62
Jon Dufresne <jon.dufresne@gmail.com>
2017-12-10 16:21:05 -0800

    Drop support for Python 2.6
    Python 2.6 and 3.3 is EOL and is no longer receiving bug fixes,
    including security fixes.
    
    https://snarky.ca/stop-using-python-2-6/
    http://www.curiousefficiency.org/posts/2015/04/stop-supporting-python26.html
    
    Fixes #133

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import find_packages, setup
 
 
-needs_pytest = set(['pytest', 'test', 'ptr']).intersection(sys.argv)
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
 
 
@@ -39,10 +39,8 @@ setup(name='chardet',
                    "Operating System :: OS Independent",
                    "Programming Language :: Python",
                    'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 2.6',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.3',
                    'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
