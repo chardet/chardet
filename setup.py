388501a88e5c3be33624a3da1ff6808a8a3d081f
Jon Dufresne <jon.dufresne@gmail.com>
2019-11-11 12:04:30 -0800

    Test Python 3.7 and 3.8 and document support (#175)
    Document that PyPy is also supported.

--- setup.py
+++ setup.py
@@ -44,6 +44,11 @@ setup(name='chardet',
                    'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   ('Programming Language :: Python :: Implementation :: '
+                    'CPython'),
+                   'Programming Language :: Python :: Implementation :: PyPy',
                    ("Topic :: Software Development :: Libraries :: Python "
                     "Modules"),
                    "Topic :: Text Processing :: Linguistic"],
