56051dddd2cbb447eb46e1e1da0288ce04fd8419
Dan Blanchard <dan.blanchard@gmail.com>
2020-12-10 16:32:38 -0500

    Clean up setup.py a little

--- setup.py
+++ setup.py
@@ -33,10 +33,7 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        (
-            "License :: OSI Approved :: GNU Library or Lesser General"
-            " Public License (LGPL)"
-        ),
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
@@ -44,9 +41,9 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        ("Programming Language :: Python :: Implementation :: " "CPython"),
+        "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
-        ("Topic :: Software Development :: Libraries :: Python " "Modules"),
+        "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Text Processing :: Linguistic",
     ],
     packages=find_packages(),
