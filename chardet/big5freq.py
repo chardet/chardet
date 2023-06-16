101497de31d75eb214ddaef975f20742cc4f4fe6
Max Mehl <mail@mehl.mx>
2023-06-16 17:13:21 +0200

    drastically reduce licensing blurb that's already covered by SPDX tags

--- chardet/big5freq.py
+++ chardet/big5freq.py
@@ -1,26 +1,7 @@
-######################## BEGIN LICENSE BLOCK ########################
-# The Original Code is Mozilla Communicator client code.
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-# 02110-1301  USA
-#
 # SPDX-License-Identifier: LGPL-2.1-or-later
 # SPDX-FileCopyrightText: 2011 - 2023 The chardet developers, see AUTHORS.rst
 # SPDX-FileCopyrightText: 1998 Netscape Communications Corporation
 # SPDX-FileContributor: Mark Pilgim (port to Python)
-######################### END LICENSE BLOCK #########################
 
 # Big5 frequency table
 # by Taiwan's Mandarin Promotion Council
